0c7421d4f060e3974997e46050975f7af973287a
Bjarne Schröder <bjasch@uni-bremen.de>
2020-07-17 14:58:04 +0200

    changes from the last days

---
+++ ParallelResponses/model/scheduling/scheduler.py
@@ -0,0 +1,80 @@
+import asyncio
+from datetime import datetime
+from typing import List, Callable, Dict
+
+from model.scheduling.Job import Job
+
+from model.database.db_handler import DatabaseHandler
+from model.exchange.exchange import Exchange
+from model.database.tables import ExchangeCurrencyPair
+
+
+class Scheduler:
+    database_handler: DatabaseHandler
+    job_list: List[Job]
+
+    def __init__(self, database_handler: DatabaseHandler, job_list: List[Job]):
+        self.database_handler = database_handler
+        self.job_list = job_list
+
+    async def run(self):
+        for job in self.job_list:
+            request = self.determine_task(job.request_name)
+            await request(job.exchanges_with_pairs)
+
+    def determine_task(self, request_name: str) -> Callable:
+        possible_requests = {
+            "ticker": self.get_tickers,
+            "historic_rates": self.get_currency_pairs,
+            "currency_pairs": self.get_currency_pairs
+        }
+        return possible_requests.get(request_name, lambda: "Invalid request name.")
+
+    async def get_tickers(self, exchanges_with_pairs: Dict[Exchange, List[ExchangeCurrencyPair]]):
+        print('Starting to collect ticker.')
+        start_time = datetime.utcnow()
+        responses = await asyncio.gather(*(ex.request('ticker', start_time, exchanges_with_pairs[ex]) for ex in exchanges_with_pairs.keys()))
+
+        for response in responses:
+            if response:
+                # print('Response: {}'.format(response))
+                exchange_name = response[0]
+                for exchange in exchanges_with_pairs.keys():
+                    if exchange.name.upper() == exchange_name.upper():
+                        break
+                formatted_response = exchange.format_ticker(response)
+                if formatted_response:
+                    self.database_handler.persist_tickers(exchanges_with_pairs[exchange], formatted_response)
+        print('Done collecting ticker.')
+
+    async def get_historic_rates(self, exchanges: [Exchange]):
+        print('Starting to collect historic rates.')
+        for ex in exchanges:
+            curr_exchange: Exchange = exchanges[ex]
+
+            # Setting Currency-Pairs
+            all_currency_pairs: [ExchangeCurrencyPair] = self.database_handler.get_all_exchange_currency_pairs(
+                curr_exchange.name)
+            curr_exchange.exchange_currency_pairs = all_currency_pairs
+
+            # Getting Historic Rates
+            hr_response = await curr_exchange.request_historic_rates('historic_rates',
+                                                                     curr_exchange.exchange_currency_pairs)
+            if hr_response is not None:
+                formatted_hr_response = curr_exchange.format_historic_rates(hr_response)
+                self.database_handler.persist_historic_rates(formatted_hr_response)
+
+        print('Done collecting historic rates.')
+
+    async def get_currency_pairs(self, exchanges: Dict[str, Exchange]):
+        print('Starting to collect currency pairs.')
+        responses = await asyncio.gather(
+            *(exchanges[ex].request_currency_pairs('currency_pairs') for ex in exchanges))
+
+        for response in responses:
+            current_exchange = exchanges[response[0]]
+            if response[1] is not None:
+                currency_pairs = current_exchange.format_currency_pairs(response)
+                self.database_handler.persist_exchange_currency_pairs(currency_pairs)
+
+        print('Done collecting currency pairs.')
