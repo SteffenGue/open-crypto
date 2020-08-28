42e9153361e8393d2f281e0fec739b780f706e08
boettger <boettger@uni-bremen.de>
2020-08-28 11:47:10 +0200

    mini fix for currency pairs request

--- ParallelResponses/main.py
+++ ParallelResponses/main.py
@@ -21,13 +21,12 @@ async def initialize_jobs(database_handler: DatabaseHandler, job_config: Dict, u
             # TODO: Error, wenn yaml nicht existiert
             exchange: Exchange = Exchange(yaml_loader(exchange_name))
 
-            if update_cp:
+            if update_cp & ('currency_pairs' != job_params['yaml_request_name']):
                 print("Updating Currency Pairs: {}".format(exchange_name))
                 response = await exchange.request_currency_pairs('currency_pairs')
                 if response[1] is not None:
                     formatted_response = exchange.format_currency_pairs(response)
                     database_handler.persist_exchange_currency_pairs(formatted_response)
-            # todo : all currency pairs abfrage
             exchange_currency_pairs: List[ExchangeCurrencyPair] = database_handler.collect_exchanges_currency_pairs(
                 exchange.name,
                 job_params['currency_pairs'],
@@ -61,8 +60,10 @@ async def main(database_handler: DatabaseHandler):
 
     jobs = await initialize_jobs(database_handler, read_config('jobs'), read_config('updates')['update_currency_pairs'])
     sched = Scheduler(database_handler, jobs)
-    # TODO: minutes aus der config holen
-    timeout_in_minutes = 600
+    timeout_in_minutes = None
+    for j in jobs:
+        timeout_in_minutes = j.frequency
+    timeout_in_minutes *= 60
     while True:
         await asyncio.gather(sched.run(), asyncio.sleep(timeout_in_minutes))
 
