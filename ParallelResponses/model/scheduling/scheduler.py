3596d47d0207a81fd7bb8068326be173ebca91a5
Steffen <stguen@uni-bremen.de>
2020-12-12 17:44:00 +0100

    neue exchange exmo

--- ParallelResponses/model/scheduling/scheduler.py
+++ ParallelResponses/model/scheduling/scheduler.py
@@ -2,9 +2,7 @@ import asyncio
 import logging
 from datetime import datetime
 from typing import List, Callable, Dict
-
 from model.scheduling.Job import Job
-
 from model.database.db_handler import DatabaseHandler
 from model.exchange.exchange import Exchange
 from model.database.tables import ExchangeCurrencyPair, Ticker, HistoricRate, OrderBook, OHLCVM, Trade
@@ -27,13 +25,13 @@ class Scheduler:
         """
         Initializer for a Scheduler.
 
-        @param database_handler: DatabaseHandler
+        :param database_handler: DatabaseHandler
             Handler that is called for everything that needs information from the database.
-        @param job_list: List[Job]
+        :param job_list: List[Job]
             List of Jobs. A job can be created with the specific yaml-template in config.yaml
-        @param frequency: int
+        :param frequency: int
             The interval in minutes with that the run() method gets called.
-        @param validated: bool
+        :param validated: bool
             Bool if the job_list has been validated. Default: False.
         """
         self.database_handler = database_handler
@@ -63,7 +61,7 @@ class Scheduler:
     async def run(self, job: Job):
         """
         The method represents one execution of the given job.
-        @param job: Job
+        :param job: Job
             The job that will be executed.
         """
 
@@ -76,13 +74,13 @@ class Scheduler:
 
         await request_fun(job.request_name, request_table, job.exchanges_with_pairs)
 
-    def remove_invalid_jobs(self, jobs: List):
+    def remove_invalid_jobs(self, jobs: List[Job]):
         """
         Method to clean up the job list. If the job list is empty, shut down program.
         Else the algorithm will go through every job specification and delete empty jobs or exchanges.
 
-        @param jobs: List of all jobs specified by the config
-        @return: List of jobs, cleaned by empty or invalid jobs
+        :param jobs: List of all jobs specified by the config
+        :return: List of jobs, cleaned by empty or invalid jobs
         """
 
         if jobs:
@@ -130,7 +128,7 @@ class Scheduler:
         any "exchange_with_pairs" or no currency_pair for an exchange, the job is removed from the list.
         This happens of the user specified currency-pairs in the config but an exchange does not offer that pair.
 
-        @return: New job_list without empty job and sets self.validated: True if the validation is successful.
+        :return: New job_list without empty job and sets self.validated: True if the validation is successful.
         """
 
         self.job_list = await self.get_currency_pairs(self.job_list)
@@ -141,9 +139,9 @@ class Scheduler:
         """
         Returns the method that is to execute based on the given request name.
 
-        @param request_name: str
+        :param request_name: str
             Name of the request.
-        @return:
+        :return:
             Method for the request name or a string that the request is false.
         """
         possible_requests = {
@@ -168,9 +166,21 @@ class Scheduler:
         }
         return possible_requests.get(request_name, lambda: "Invalid request name.")
 
-    async def get_currency_pairs(self, job_list):
+    async def get_currency_pairs(self, job_list: List[Job]) -> List[Job]:
+        """
+        Method to get all exchange currency pairs. First the database is queried, if the result is [], the exchanges
+        api for all currency pairs is called.
 
-        async def update_currency_pairs(exchange):
+        :param job_list: list of all jobs, including Job-Objects
+        :return job_list with updated exchange_currency_pairs
+        """
+
+        async def update_currency_pairs(exchange: Exchange):
+            """
+            This method requests the currency_pairs.
+
+            :param exchange: Current exchange.
+            """
             response = await exchange.request_currency_pairs()
             if response[1]:
                 formatted_response = exchange.format_currency_pairs(response)
@@ -194,7 +204,6 @@ class Scheduler:
                     job_params['first_currencies'],
                     job_params['second_currencies']
                 )
-
         return job_list
 
     async def get_job_done(self,
