2727cbd7d12d2641bd0b3fadeeb80a6d3e09e72f
boettger <boettger@uni-bremen.de>
2020-12-28 20:12:27 +0100

    added unit tests for get_all_currency_pairs_from_exchange and get_currency_pairs_with_first / second_currency

--- ParallelResponses/tests/unit_tests/test_persist_response.py
+++ ParallelResponses/tests/unit_tests/test_persist_response.py
@@ -23,7 +23,7 @@ class TestPersistResponse(unittest.TestCase):
     session = db_handler.sessionFactory()
     currencies = ['BTC', 'ETH', 'LTC', 'XRP', 'DIO', 'DASH']
     currency_pairs = permutations(currencies, 2)
-    exchange_currency_pairs = [('TESTEXCHANGE', ) + pair for pair in currency_pairs]
+    exchange_currency_pairs = [('TESTEXCHANGE',) + pair for pair in currency_pairs]
     db_handler.persist_exchange_currency_pairs(exchange_currency_pairs,
                                                is_exchange=True)
 
@@ -32,16 +32,14 @@ class TestPersistResponse(unittest.TestCase):
         result = [(item.exchange_name, item.first_name, item.second_name) for item in result]
         self.assertEqual(self.exchange_currency_pairs, result)
 
-
-
     def test_persist_valid_ticker(self):
         response = [
             (datetime.utcnow(), datetime.utcnow(), 1.0, 1.0, 1.0, 1.0, 1),
             (datetime.utcnow(), datetime.utcnow(), 2.0, 2.0, 2.0, 2.0, 2),
             (datetime.utcnow(), datetime.utcnow(), 3.0, 3.0, 3.0, 3.0, 3),
-            (datetime.utcnow(), datetime.utcnow(), 4.0, 4.0, 4.0, 4.0, 4),]
+            (datetime.utcnow(), datetime.utcnow(), 4.0, 4.0, 4.0, 4.0, 4), ]
         exchanges_with_pairs = {self.session.query(Exchange).first():
-                                list(self.session.query(ExchangeCurrencyPair).limit(4))}
+                                    list(self.session.query(ExchangeCurrencyPair).limit(4))}
 
         exchange = list(exchanges_with_pairs.keys())[0]
         request_name = Ticker.__tablename__
@@ -65,15 +63,16 @@ class TestPersistResponse(unittest.TestCase):
                    item.best_bid,
                    item.daily_volume) for item in result]
 
-        response2 = [(ExCuPair[0], ExCuPair[1], ExCuPair[2]) + res for (ExCuPair, res) in zip(self.exchange_currency_pairs, response)]
+        response2 = [(ExCuPair[0], ExCuPair[1], ExCuPair[2]) + res for (ExCuPair, res) in
+                     zip(self.exchange_currency_pairs, response)]
         self.assertEqual(response2, result)
         self.session.query(Ticker).delete()
 
     def test_persist_ticker_with_no_pk(self):
         response = [
-            (datetime.utcnow(),  1.0, 1.0, 1.0, 1.0, 1.0),]
+            (datetime.utcnow(), 1.0, 1.0, 1.0, 1.0, 1.0), ]
         exchanges_with_pairs = {self.session.query(Exchange).first():
-                                list(self.session.query(ExchangeCurrencyPair).limit(4))}
+                                    list(self.session.query(ExchangeCurrencyPair).limit(4))}
 
         exchange = list(exchanges_with_pairs.keys())[0]
         request_name = Ticker.__tablename__
@@ -82,7 +81,6 @@ class TestPersistResponse(unittest.TestCase):
         self.assertRaises(NotAllPrimaryKeysException, self.db_handler.persist_response, exchanges_with_pairs,
                           exchange, Ticker, response, mappings)
         self.session.query(Ticker).delete()
-
 
     def test_persist_response_with_unknown_column(self):
         response = [
@@ -95,7 +93,8 @@ class TestPersistResponse(unittest.TestCase):
 
         exchange = list(exchanges_with_pairs.keys())[0]
         request_name = Ticker.__tablename__
-        mappings = ['start_time', 'time', 'best_ask', 'best_bid', 'last_price', 'daily_volume', 'exchange_pair_id', 'some_column']
+        mappings = ['start_time', 'time', 'best_ask', 'best_bid', 'last_price', 'daily_volume', 'exchange_pair_id',
+                    'some_column']
 
         self.db_handler.persist_response(exchanges_with_pairs,
                                          exchange,
@@ -120,7 +119,6 @@ class TestPersistResponse(unittest.TestCase):
         self.session.query(Ticker).delete()
 
     def test_persist_response_with_unknown_currency_pair(self):
-
         response = [
             ("TEST1", "TEST2", datetime.utcnow(), datetime.utcnow(), 1.0, 1.0, 1.0, 1.0, 1)]
         exchanges_with_pairs = {self.session.query(Exchange).first(): []}
@@ -135,8 +133,8 @@ class TestPersistResponse(unittest.TestCase):
                                          response,
                                          mappings)
 
-        result = self.session.query(ExchangeCurrencyPairView).filter(ExchangeCurrencyPairView.first_name=="TEST1",
-                                                                     ExchangeCurrencyPairView.second_name=="TEST2").first()
+        result = self.session.query(ExchangeCurrencyPairView).filter(ExchangeCurrencyPairView.first_name == "TEST1",
+                                                                     ExchangeCurrencyPairView.second_name == "TEST2").first()
         self.assertEqual(("TESTEXCHANGE", "TEST1", "TEST2"), (result.exchange_name,
                                                               result.first_name,
                                                               result.second_name))
@@ -177,8 +175,49 @@ class TestPersistResponse(unittest.TestCase):
         self.assertEqual(response2, result)
         self.session.query(Ticker).delete()
 
+    def test_get_all_currency_pairs_from_exchange_with_no_invalid_pair(self):
+        test_result = self.db_handler.get_all_currency_pairs_from_exchange('TESTEXCHANGE')
+        test_result = [(item.exchange_id,
+                        item.first_id,
+                        item.second_id) for item in test_result]
+        result = self.session.query(ExchangeCurrencyPair).all()
+        result = [(item.exchange_id,
+                   item.first_id,
+                   item.second_id) for item in result]
+        self.assertEqual(test_result, result)
 
+    def test_get_all_currency_pairs_from_exchange_with_invalid_pair(self):
+        self.session.query(ExchangeCurrencyPair).delete()
+        self.db_handler.persist_exchange_currency_pair('invalid', 'BTC', 'ETH', True)
+        test_result = self.db_handler.get_all_currency_pairs_from_exchange('TESTEXCHANGE')
+        result = []
+        self.assertEqual(test_result, result)
+        self.db_handler.persist_exchange_currency_pairs(self.exchange_currency_pairs,
+                                                        is_exchange=True)
 
+    def test_get_currency_pairs_with_first_currency_valid_1(self):
+        test_result = self.db_handler.get_currency_pairs_with_first_currency('TESTEXCHANGE', ['BTC'])
+        test_result = [item.first_id for item in test_result]
+        result = [1, 1, 1, 1, 1]
+        self.assertEqual(test_result, result)
 
+    def test_get_currency_pairs_with_first_currency_valid_2(self):
+        test_result = self.db_handler.get_currency_pairs_with_first_currency('TESTEXCHANGE', ['BTC', 'LTC'])
+        test_result = [item.first_id for item in test_result]
+        result = [1, 1, 1, 1, 1, 3, 3, 3, 3, 3]
+        self.assertEqual(test_result, result)
 
+    def test_get_currency_pairs_with_second_currency_valid(self):
+        #todo : Eingabeparameter in der Methode get_currency_pairs_with_second_currency in db_handler
+        #       müsste eigentlich eine Liste an Currencies entgegennehmen, wie in der Methode
+        #       get_currency_pairs_with_first_currency, und nicht nur einen einzelnen String.
+        # Ich habe das noch nicht gefixt, da ich nicht genau weiß, ob dann eventuell Fehlermeldungen geworfen, bei den vorhandenen Aufrufen der Methode. Diese Aufrufe müssten dann eventuell angepasst werden.
+        test_result = self.db_handler.get_currency_pairs_with_second_currency('TESTEXCHANGE', ['BTC'])
+        test_result = [item.second_id for item in test_result]
+        result = [1, 1, 1, 1, 1]
+        self.assertEqual(test_result, result)
 
+    def test_get_currency_pairs_with_second_currency_invalid(self):
+        test_result = self.db_handler.get_currency_pairs_with_second_currency('TESTEXCHANGE', ['BAT'])
+        result = []
+        self.assertEqual(test_result, result)
