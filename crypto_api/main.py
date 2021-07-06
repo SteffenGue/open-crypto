0def08cd39bb21f5d21e1bd6a19adeab5151223c
Philipp Schönemann <schoenemann.philipp93@gmail.com>
2021-07-06 22:38:28 +0200

    add missing type hints in main

--- crypto_api/main.py
+++ crypto_api/main.py
@@ -10,6 +10,7 @@ import logging
 import os
 import signal
 import sys
+from typing import Any
 
 from model.database.db_handler import DatabaseHandler
 from model.database.tables import metadata, ExchangeCurrencyPair
@@ -20,7 +21,7 @@ from model.utilities.time_helper import TimeHelper
 from model.utilities.utilities import read_config, yaml_loader, get_exchange_names
 
 
-def signal_handler(signal_number, stack):
+def signal_handler(signal_number: Any, stack: Any) -> None:
     """
     Helper function to exit the program. When CTRL+C is hit, the program will shut down with exit code(0).
     """
@@ -31,7 +32,10 @@ def signal_handler(signal_number, stack):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-async def initialize_jobs(job_config: dict, timeout, interval, db_handler: DatabaseHandler) -> list[Job]:
+async def initialize_jobs(job_config: dict[str, Any],
+                          timeout: int,
+                          interval: Any,
+                          db_handler: DatabaseHandler) -> list[Job]:
     """
     Initializes and creates new Job Objects and stores them in a list. There will be one Job-Object for every request
     method, independent of the amount of exchanges or currency_pairs specified in the config. The Dict
@@ -43,10 +47,10 @@ async def initialize_jobs(job_config: dict, timeout, interval, db_handler: Datab
     @param job_config: Dictionary with job parameter gathered from the config-file.
     @return: A list of Job objects.
     """
-    jobs: [Job] = list()
+    jobs: list[Job] = list()
 
     for job in job_config.keys():
-        job_params: dict = job_config[job]
+        job_params = job_config[job]
         exchange_names = job_params["exchanges"] if job_params["exchanges"][0] != "all" else get_exchange_names()
 
         if job_params.get("excluded"):
@@ -67,7 +71,7 @@ async def initialize_jobs(job_config: dict, timeout, interval, db_handler: Datab
     return jobs
 
 
-def init_logger(path):
+def init_logger(path: str) -> None:
     """
     Initializes the logger, specifies the path to the logging files, the logging massage as well as the logging level.
 
@@ -83,7 +87,7 @@ def init_logger(path):
             level=logging.ERROR)
 
 
-def handler(ex_type, ex_value, ex_traceback):
+def handler(ex_type: Any, ex_value: Any, ex_traceback: Any) -> None:
     """
     Method to catch and log unexpected exceptions.
 
@@ -94,7 +98,7 @@ def handler(ex_type, ex_value, ex_traceback):
     logging.exception("Uncaught exception: %s: %s", ex_type, ex_value)
 
 
-async def main(database_handler: DatabaseHandler, file: str = None):
+async def main(database_handler: DatabaseHandler) -> None:
     """
     The model() function to run the program. Loads the database, including the database_handler.
     The exchange_names are extracted with a helper method in utilities based on existing yaml-files.
@@ -103,7 +107,6 @@ async def main(database_handler: DatabaseHandler, file: str = None):
     As soon as all responses from the exchange are returned, the values get extracted, formatted into tuples
         by the exchange.get_ticker(..) method and persisted by the into the database by the database_handler.
 
-    @param file:
     @param: Instance of the DatabaseHandler
     @type database_handler: object
     """
@@ -139,7 +142,7 @@ async def main(database_handler: DatabaseHandler, file: str = None):
                 logging.exception(TimeHelper.now(), ex)
 
 
-def run(file: str = None, path: str = None):
+def run(file: str = None, path: str = None) -> None:
     """
     Starts the program and initializes the asyncio-event-loop.
 
