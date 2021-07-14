36f3652fd95ed91a5073569c490cf774533b84f3
Steffen <stguen@uni-bremen.de>
2021-07-14 09:52:58 +0200

    Added test_exchange.py

--- crypto_api/tests/unit_tests/test_exchange.py
+++ crypto_api/tests/unit_tests/test_exchange.py
@@ -13,57 +13,56 @@ Since:
 Version:
     07.07.2021
 """
-#
-# import oyaml as yaml
-# import pytest
-# from model.exchange.exchange import Exchange
-#
-# with open("test_file.yaml", "r") as file:
-#     test_file: dict = yaml.load(file, Loader=yaml.FullLoader)
-#
-#
-# class TestExchange:
-#     """Test class for Exchange."""
-#     # pylint: disable=too-many-public-methods
-#
-#     Exchange = Exchange(test_file, None, timeout=10, interval='seconds')
-#
-#     def test_increase_interval(self):
-#         """
-#         Test function to increase interval from "seconds" to "minutes".
-#         """
-#         index = 2
-#         self.Exchange.interval = self.Exchange.interval_strings[index]
-#         self.Exchange.increase_interval()
-#         assert self.Exchange.interval == self.Exchange.interval_strings[index+1]
-#
-#     def test_increase_max_interval(self):
-#         """
-#         Test that function can not increase interval higher "days".
-#         """
-#         index = - 1
-#         self.Exchange.interval = self.Exchange.interval_strings[index]
-#         self.Exchange.increase_interval()
-#         assert self.Exchange.interval == self.Exchange.interval_strings[index]
-#
-#     def test_decrease_interval(self):
-#         """
-#         Test function that decreases interval from "hours" to "minutes".
-#         """
-#         index = 2
-#         self.Exchange.interval = self.Exchange.interval_strings[index]
-#         self.Exchange.decrease_interval()
-#         assert self.Exchange.interval == self.Exchange.interval_strings[index - 1]
-#
-#     def test_decrease_min_interval(self):
-#         """
-#         Test that function cannot decrease interval lower than "seconds".
-#         """
-#         index = 0
-#         self.Exchange.interval = self.Exchange.interval_strings[index]
-#         self.Exchange.decrease_interval()
-#         assert self.Exchange.interval == self.Exchange.interval_strings[index]
-#
+
+import oyaml as yaml
+from model.exchange.exchange import Exchange
+
+with open("test_file.yaml", "r") as file:
+    test_file: dict = yaml.load(file, Loader=yaml.FullLoader)
+
+
+class TestExchange:
+    """Test class for Exchange."""
+    # pylint: disable=too-many-public-methods
+
+    Exchange = Exchange(test_file, None, timeout=10, interval='seconds')
+
+    def test_increase_interval(self) -> None:
+        """
+        Test function to increase interval from "seconds" to "minutes".
+        """
+        index = 2
+        self.Exchange.interval = self.Exchange.interval_strings[index]
+        self.Exchange.increase_interval()
+        assert self.Exchange.interval == self.Exchange.interval_strings[index+1]
+
+    def test_increase_max_interval(self) -> None:
+        """
+        Test that function can not increase interval higher "days".
+        """
+        index = - 1
+        self.Exchange.interval = self.Exchange.interval_strings[index]
+        self.Exchange.increase_interval()
+        assert self.Exchange.interval == self.Exchange.interval_strings[index]
+
+    def test_decrease_interval(self) -> None:
+        """
+        Test function that decreases interval from "hours" to "minutes".
+        """
+        index = 2
+        self.Exchange.interval = self.Exchange.interval_strings[index]
+        self.Exchange.decrease_interval()
+        assert self.Exchange.interval == self.Exchange.interval_strings[index - 1]
+
+    def test_decrease_min_interval(self) -> None:
+        """
+        Test that function cannot decrease interval lower than "seconds".
+        """
+        index = 0
+        self.Exchange.interval = self.Exchange.interval_strings[index]
+        self.Exchange.decrease_interval()
+        assert self.Exchange.interval == self.Exchange.interval_strings[index]
+
 #     # def test_format_request_url(self):
 #     #     """
 #     #     # ToDo
